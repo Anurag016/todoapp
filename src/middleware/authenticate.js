d69b9183d9f9bfaa54815040b8475342a41ec65c
User <sanurag789@gmail.com>
2024-10-24 02:38:18 +0530

    Implemented Middleware to Check JWT token for Todo CRUD APIs

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -1,7 +1,27 @@
 const constant = require('../config/const');
+const userModel = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 async function authenticate(req, res, next) {
     const token = req.headers.token;
-    next();
+    // Verify the token
+    if (!token) {
+        return res.status(401).json({ message: 'Authentication failed: No token provided.' });
+    }
+
+    jwt.verify(token, constant.JWTSECRET, async (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ message: 'Authentication failed: Invalid token.' });
+        }
+        console.log("Decoded: ", decoded);
+        let userDetail = await userModel.findOne({
+            userId: decoded.userId
+        });
+        if (!userDetail) {
+            return res.status(401).json({ message: 'Authentication failed: Invalid token.' });
+        }
+        req.userId = userDetail.userId;
+        next();
+    });
 }
 module.exports = authenticate;
